a3001a5307434d1cd54aaafdb939105309fed547
weider86 <weider.lima@mercadolivre.com>
2021-07-02 12:44:23 -0300

    project1 initial implementation

---
+++ src/app/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from '../../components/Header';
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <div>
+        <p>Project 1</p>
+      </div>
+    </>
+  );
+};
+
+export { App };
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = (): JSX.Element => (
+  <div>
+    <h1>Project 1</h1>
+  </div>
+);
+
+export { Header };
